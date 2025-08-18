8ef5c0d0ddf3377a37bf0d327af0a03c305cef53
Hanaakp <fathmahanalp@gmail.com>
2025-08-19 00:59:09 +0530

    connect functionality issue fixed v2

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -14,7 +14,7 @@ const getUserToken = () => {
 
 const socketURL =
   import.meta.env.VITE_SOCKET_URL ||
-  (import.meta.env.DEV ? 'http://localhost:5000' : '');
+  (import.meta.env.DEV ? 'http://localhost:5000' : window.location.origin);
 
 const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
