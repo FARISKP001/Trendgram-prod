4d41079d40193405db3132399f9217f211d1022b
FARISKP001 <fariskarikkumpurath@gmail.com>
2025-09-07 23:31:23 +0530

    Update main.jsx
    UI fixes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import SocketProvider from './context/SocketProvider';
-import { ThemeProvider } from './context/ThemeContext';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <SocketProvider>
-      <ThemeProvider>
         <App />
-      </ThemeProvider>
     </SocketProvider>
   </React.StrictMode>
 );
