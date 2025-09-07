8d3d30398ede6f5b9a33fdc7d5fab797399d801a
FARISKP001 <fariskarikkumpurath@gmail.com>
2025-09-07 17:50:44 +0530

    Update HomePage.jsx
    Age confirmation issue fixed

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -523,7 +523,7 @@ const HomePage = () => {
       </footer>
 
 {showAgeModal && (
-  <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+  <div className="mb-4">
     <AgeConfirmation onConfirm={handleAgeConfirm} onCancel={handleAgeCancel} />
   </div>
 )}
