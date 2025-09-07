5926203313fff1aa750a77845e487845d7646b5c
FARISKP001 <fariskarikkumpurath@gmail.com>
2025-09-07 23:29:59 +0530

    Update AgeConfirmation.jsx
    UI Fixes

--- client/src/components/AgeConfirmation.jsx
+++ client/src/components/AgeConfirmation.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 
 const AgeConfirmation = ({ onConfirm, onCancel }) => {
   return (
-    <div className="w-full max-w-[400px] mt-1 bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-100 rounded-2xl shadow-lg p-3 text-center">
-      <h2 className="text-base font-semibold mb-0.5 text-[#c46210]">Age Confirmation</h2>
-      <p className="text-xs mb-2 text-[#c46210] leading-tight">
+    <div className=" max-w-[400px] mt-1 bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-100 rounded-3xl p-4 text-center">
+      <h2 className="text-2xl font-mono font-bold mb-6 text-[#242124]">Age Confirmation</h2>
+      <p className="text-md font-mono mb-8 text-[#242124] leading-tight">
         You must be at least 18 years old to use this site. Are you 18 or older?
       </p>
-      <div className="flex justify-center gap-3">
+      <div className="flex justify-center gap-10">
         <button
-          className="px-3 py-1 rounded-full bg-[#8fbc8f] hover:bg-[#8fbc8f]/80 text-white text-sm"
+          className="px-4 py-2 font-mono font-bold rounded-full bg-[#00008b] /80 text-white text-sm"
           onClick={onConfirm}
         >
-          Yes
+          Proceed
         </button>
         <button
-          className="px-3 py-1 rounded-full bg-[#ff7f50] hover:bg-[#ff7f50]/80 text-white text-sm"
+          className="px-4 py-2 font-mono font-bold rounded-full bg-[#87ceeb] /80 text-white text-sm"
           onClick={onCancel}
         >
-          No
+          Cancel
         </button>
       </div>
     </div>
