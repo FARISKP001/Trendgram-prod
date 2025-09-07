30b8fae323efee007ec6d97de4bd6f8ebf954e08
FARISKP001 <fariskarikkumpurath@gmail.com>
2025-09-08 01:03:59 +0530

    Update AgeConfirmation.jsx
    UI Fixes

--- client/src/components/AgeConfirmation.jsx
+++ client/src/components/AgeConfirmation.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AgeConfirmation = ({ onConfirm, onCancel }) => {
   return (
-    <div className=" max-w-[400px] mt-1 bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-100 rounded-3xl p-4 text-center">
+    <div className=" max-w-[500px] mt-1 bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-100 rounded-3xl p-4 text-center">
       <h2 className="text-2xl font-mono font-bold mb-6 text-[#242124]">Age Confirmation</h2>
       <p className="text-md font-mono mb-8 text-[#242124] leading-tight">
         You must be at least 18 years old to use this site. Are you 18 or older?
